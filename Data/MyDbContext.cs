299607406dc0db7b15926c0a0feb2662683d0297
TW\S434977 <s434977@sinyi.com.tw>
2025-04-28 14:35:12 +0800

    第一次作業提交

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Homework_SkillTree.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework_SkillTree.Respository
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<LedgerModel> Ledger { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LedgerModel>(entity =>
+            {
+                entity.Property(x => x.Id).ValueGeneratedOnAdd();
+            });
+        }
+
+    }
+}
---
+++ Migrations/20250428060358_Init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Homework_SkillTree.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ledger",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LedgerCategory = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: false),
+                    LedgerAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    LedgerNote = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    LedgerDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ledger", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ledger");
+        }
+    }
+}
